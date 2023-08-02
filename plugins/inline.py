bc7c113082a5360dafc2340f2ba343bbb67f0d9e
Toxic <137384106+ToxicBois44@users.noreply.github.com>
2023-08-02 15:35:10 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -31,8 +31,8 @@ from pyrogram import (
 
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://github.com/subinps/VCPlayerBot'),
-        InlineKeyboardButton('🧩 Join Here', url='https://t.me/subin_works'),
+        InlineKeyboardButton('✨ 𝓢𝓤𝓟𝓟𝓞𝓡𝓣 ✨', url='https://t.me/CineFlixLK'),
+        InlineKeyboardButton('⚡️ 𝓒𝓘𝓝𝓔 𝓕𝓛𝓘𝓧 ⚡️', url='https://t.me/CineFlixFinderBot'),
     ]
     ]
 def get_cmd(dur):
@@ -47,7 +47,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/VCPlayerBot) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{Config.REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/ToxicBois44/VcplayerBot) below.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
