6e9c4fe1fc2153b68df4ef4ce4851b627addeafb
Toxic <137384106+ToxicBois44@users.noreply.github.com>
2023-08-02 15:40:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -129,8 +129,8 @@ async def start(client, message):
         return
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot')
+            InlineKeyboardButton('✨ 𝓢𝓤𝓟𝓟𝓞𝓡𝓣 ✨', url='https://t.me/Cineflixlk'),
+            InlineKeyboardButton('⚡️ 𝓒𝓘𝓝𝓔 𝓕𝓛𝓘𝓧 ⚡️', url='https://t.me/CineFlixFinderBot')
         ],
         [
             InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help_main'),
@@ -188,15 +188,15 @@ async def show_help(client, message):
 async def repo_(client, message):
     buttons = [
         [
-            InlineKeyboardButton('🧩 Repository', url='https://github.com/subinps/VCPlayerBot'),
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),     
+            InlineKeyboardButton('✨ 𝓢𝓤𝓟𝓟𝓞𝓡𝓣 ✨', url='https://t.me/CineFlixlk'),
+            InlineKeyboardButton('⚡️ 𝓒𝓘𝓝𝓔 𝓕𝓛𝓘𝓧 ⚡️', url='https://t.me/CineFlixFinderBot'),     
         ],
         [
             InlineKeyboardButton("🎞 How to Deploy", url='https://youtu.be/mnWgZMrNe_0'),
             InlineKeyboardButton('🗑 Close', callback_data='close'),
         ]
     ]
-    await message.reply("<b>The source code of this bot is public and can be found at <a href=https://github.com/subinps/VCPlayerBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.\n\nFeel free to star☀️ the repo if you liked it 🙃.</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply("<b>The source code of this bot is public and can be found at <a href=https://github.com/ToxicBois44/VcplayerBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.\n\nFeel free to star☀️ the repo if you liked it 🙃.</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
     await delete_messages([message])
 
 @Client.on_message(filters.command(['restart', 'update', f"restart@{Config.BOT_USERNAME}", f"update@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
